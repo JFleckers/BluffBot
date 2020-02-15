cceff0ea8ec76368ab94e572a6f7cf9f74a75687
gabek415 <60748873+gabek415@users.noreply.github.com>
2020-02-14 20:02:56 -0500

    Create LiarsDice.py

---
+++ LiarsDice.py
@@ -0,0 +1,23 @@
+import random
+
+class Die:
+    def __init__(self):
+        self.die = random.randint(1,6)
+    
+class Cup:    
+    
+    def __init__(self):
+        self.cup = []
+        self.count = 5
+        
+    def roll(self):
+        self.cup.clear()
+        x = 0
+        while x <= 5:
+            self.cup.append(Die())
+            x += 1
+
+c = Cup()
+c.roll()
+for i in c.cup:
+    print(i.die)
